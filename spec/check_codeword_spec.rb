7f3c59d0c784910439cee50010404cd1bae21912
David Scott <sirdavidscott@hotmail.com>
2022-12-05 15:28:38 +0000

    check_codeword 1

---
+++ spec/check_codeword_spec.rb
@@ -0,0 +1,15 @@
+require 'check_codeword'
+RSpec.describe "check_codewood method"
+  it "tests codeword" do
+    result = check_codeword(horse)
+    expect(result).to eq "Correct! Come in."
+  end
+  it "tests codeword2" do
+    result = check_codeword(house)
+    expect(result).to eq "Close, but nope."
+  end
+  it "tests codeword3" do
+    result = check_codeword(pineapple) 
+    expect(result).to eq "WRONG!"
+  end
+end
