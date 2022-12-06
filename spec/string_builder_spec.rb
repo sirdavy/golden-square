f988f0a9eb98315766f1c5a11b4c2363ae05c8a4
Marta Bianchini <marta.bianchini24@gmail.com>
2022-12-06 11:43:43 +0000

    Added another test for .size

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -22,4 +22,12 @@ RSpec.describe StringBuilder do
     result = example_string.output
     expect(result).to eq "Hello, Marta"
   end
+
+  it "build a string" do
+    example_string = StringBuilder.new
+    example_string.add("Hello, ")
+    example_string.add("Marta")
+    result = example_string.size
+    expect(result).to eq 12
+  end
 end
