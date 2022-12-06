5cc4c96587a624f3a6d05d3c223e30b2bb74a768
David Scott <sirdavidscott@hotmail.com>
2022-12-06 10:56:27 +0000

    Counter exercise

---
+++ spec/counter_spec.rb
@@ -0,0 +1,9 @@
+require 'counter'
+Rspec.describe Counter do
+  it "counts the number you've added" do
+    counter = Counter.new
+    counter.add(9)
+    result = counter.report()
+    expect(result).to eq "Counted to 9 so far."
+  end
+end
