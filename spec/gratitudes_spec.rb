28e604ca3b23b8f729204524730528b062ca6b83
David Scott <sirdavidscott@hotmail.com>
2022-12-06 16:08:27 +0000

    improved gratitudes rspec

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -1,5 +1,19 @@
 require 'gratitudes'
+
 RSpec.describe Gratitudes do
+  #code below checks that the array created is empty .instance_variable_get(:@gratitudes)
+  it "returns an empty array" do
+    example_grat = Gratitudes.new 
+    expect(example_grat.instance_variable_get(:@gratitudes)).to eq []
+  end
+
+  it "adds string to array" do
+    example_grat = Gratitudes.new 
+    example_grat.add("good health")
+    expect(example_grat.instance_variable_get(:@gratitudes)).to eq ["good health"]
+  end
+
+
   it "compiles array of gratitudes" do
     example_grat = Gratitudes.new
     example_grat.add("good health")
@@ -7,4 +21,5 @@ RSpec.describe Gratitudes do
     result = example_grat.format
     expect(result).to eq "Be grateful for: good health, nice weather"
   end
+
 end
