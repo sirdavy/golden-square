ea361bad5205494357f0f4ac3b8cc70e230219d0
David Scott <sirdavidscott@hotmail.com>
2022-12-29 15:33:15 +0000

    folder clean up test

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe PasswordChecker do
 
   it "returns error for password <8 characters" do
     examp_password = PasswordChecker.new
-    # examp_password.check("book")
     expect { examp_password.check("book") }.to raise_error "Invalid password, must be 8+ characters."
   end
 end
